905118d92ae2c4d0051a1f743eea8be1d1807ee7
motto <974444718@qq.com>
2016-12-26 13:39:28 +0800

    生产环境打包启动

---
+++ prod-server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var config = require('./config/index.js');
+
+var port = process.env.PORT || config.build.port;
+
+var app = express();
+
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+  req.url = '/index.html';
+  next();
+});
+
+app.use(router);
+
+var appData = require('./data.json');
+
+var apiRouter = express.Router();
+
+apiRouter.get('/seller', function (req, res) {
+  res.json({code: 0, msg: 'ok', seller: appData.seller});
+});
+
+apiRouter.get('/goods', function (req, res) {
+  res.json({code: 0, msg: 'ok', goods: appData.goods});
+});
+
+apiRouter.get('/ratings', function (req, res) {
+  res.json({code: 0, msg: 'ok', ratings: appData.ratings});
+});
+
+app.use('/api', apiRouter);
+
+app.use(express.static('./dist'));
+
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  var uri = 'http://localhost:' + port;
+  console.log('Listening at ' + uri + '\n');
+
+  // when env is testing, don't need open it
+  if (process.env.NODE_ENV !== 'testing') {
+    // opn(uri)
+  }
+});
